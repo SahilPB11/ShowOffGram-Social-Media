75908a419159735320ee38ff86d3349b965b953f
Sahil <garg07825@gmail.com>
2023-11-02 15:05:16 +0530

    Create Home and Form routes successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Routes, Route } from "react-router-dom"
 import "./globals.css"
+import SignInForm from "./_Auth/forms/SignInForm"
+import SignUpForm from "./_Auth/forms/SignUpForm"
+import AuthLayout from "./_Auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
+import { Home } from "./_root/pages/Index"
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <main className="flex h-screen">
+            <Routes>
+                {/* public Routes */}
+                <Route element={<AuthLayout />} >
+                    <Route path="/sign-in" element={<SignInForm />} />
+                    <Route path="/sign-up" element={<SignUpForm />} />
+                </Route>
+
+                {/* Private Routes */}
+                <Route element={<RootLayout />} >
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
     )
 }
 
---
+++ src/_Auth/forms/SignInForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignInForm = () => {
+    return (
+        <div>
+            SignInForms
+        </div>
+    )
+}
+
+export default SignInForm
