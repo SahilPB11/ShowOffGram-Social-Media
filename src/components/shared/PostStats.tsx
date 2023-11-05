8fcf0bcb219386bb15226521adf6a4668d7e5742
Sahil <garg07825@gmail.com>
2023-11-05 15:26:17 +0530

    like and save fun work perfectly Poststate finish

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,24 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from '@/lib/react-query/queriesAndMutation';
 import { Models } from 'appwrite'
+import { checkIsLiked } from '@/lib/utils';
+import Loader from './Loader';
 type PostStatsProps = {
     post: Models.Document;
     userId: string
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
+    const likeList = post.likes.map((user: Models.Document) => user.$id);
+
+    const [likes, setLikes] = useState(likeList);
+    const [isSaved, setIsSaved] = useState(false);
+
+    const { mutate: likePost } = useLikePost();
+    const { mutate: savePost, isPending: isSavingPost } = useSavePost();
+    const { mutate: deleteSavedPost, isPending: isDeletingSavedPost } = useDeleteSavedPost();
+
+    const { data: currentUser } = useGetCurrentUser();
+
+    const savePostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post.$id)
+
+    useEffect(() => {
+        setIsSaved(savePostRecord ? true : false)
+    }, [currentUser])
+
+    const handleLikePost = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        let newLikes = [...likes];
+
+        const hasLiked = newLikes.includes(userId);
+
+        if (hasLiked) {
+            newLikes = newLikes.filter((id) => id !== userId);
+        } else {
+            newLikes.push(userId);
+        }
+        setLikes(newLikes);
+        likePost({ postId: post.$id, likesArray: newLikes })
+    }
+
+    const handleSavePost = (e: React.MouseEvent) => {
+        e.stopPropagation();
+
+        if (savePostRecord) {
+            setIsSaved(false);
+            deleteSavedPost(savePostRecord.$id);
+        } else {
+            savePost({ postId: post.$id, userId });
+            setIsSaved(true);
+        }
+    }
     return (
         <div className='flex justify-between items-center z-20'>
             <div className="flex gap-2 mr-5">
-                <img src="/assets/icons/like.svg" alt='like' width={20} height={20}
-                    onClick={() => { }}
+                <img src={checkIsLiked(likes, userId) ? "/assets/icons/liked.svg" : "/assets/icons/like.svg"}
+                    alt='like'
+                    width={20}
+                    height={20}
+                    onClick={handleLikePost}
                     className='cursor-pointer'
                 />
-                <p className='small-medium lg:base-medium'>0</p>
+                <p className='small-medium lg:base-medium'>{likes.length}</p>
             </div>
             <div className="flex gap-2 ">
-                <img src="/assets/icons/save.svg" alt='like' width={20} height={20}
-                    onClick={() => { }}
+                {isSavingPost || isDeletingSavedPost ? <Loader /> : <img src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
+                    alt='like'
+                    width={20}
+                    height={20}
+                    onClick={handleSavePost}
                     className='cursor-pointer'
-                />
+                />}
             </div>
         </div>
     )
--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -19,7 +19,7 @@ const TopBar = () => {
 
                 </Link>
                 <div className="flex gap-4">
-                    <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut} >
+                    <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()} >
                         <img src="/assets/icons/logout.svg" alt="logout" />
                     </Button>
                     <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
