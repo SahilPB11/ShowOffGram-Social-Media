9f8a9123c35887be383d76492b11a23a2b886090
Sahil <garg07825@gmail.com>
2023-11-08 22:59:21 +0530

    Added a sie panel of top creator in home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,10 +1,25 @@
 import Loader from "@/components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
-import { useGetRecentPost } from "@/lib/react-query/queriesAndMutation";
+import UserCard from "@/components/shared/UserCard";
+import { useGetRecentPost, useGetAllusers } from "@/lib/react-query/queriesAndMutation";
 import { Models } from "appwrite";
 
 const Home = () => {
     const { data: posts, isPending: isPostLoading, isError: isErrorPost } = useGetRecentPost();
+    const { data: AllUsers, isPending: isUsersPending, isError: isErrorUsers } = useGetAllusers();
+
+    if (isErrorPost || isErrorUsers) {
+        return (
+            <div className="flex flex-1">
+                <div className="home-container">
+                    <p className="body-medium text-light-1">Something Bad Happend</p>
+                </div>
+                <div className="home-creators">
+                    <p className="body-medium text-light-1">Something Bad Happend</p>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className="flex flex-1">
@@ -16,11 +31,28 @@ const Home = () => {
                     ) : (
                         <ul className="flex flex-col flex-1 gap-9 w-full">
                             {posts?.documents.map((post: Models.Document) => (
-                                <PostCard post={post} key={post.$id} />
+                                <li key={post.$id} className="flex justify-center w-full">
+                                    <PostCard post={post} />
+                                </li>
                             ))}
                         </ul>
                     )}
                 </div>
+            </div>
+
+            <div className="home-creators">
+                <h3 className="h3-bold text-light-1">Top Creators</h3>
+                {isUsersPending && !AllUsers ? (
+                    <Loader />
+                ) : (
+                    <ul className="grid 2xl:grid-cols-2 gap-6">
+                        {AllUsers?.documents.map((creator) => (
+                            <li key={creator?.$id}>
+                                <UserCard user={creator} />
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
 
         </div>
